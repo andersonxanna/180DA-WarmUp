0ff1e954918fc0d39c6c37046a2d896a85358c02
andersonxanna <annaranderson@icloud.com>
2023-01-12 15:45:37 -0800

    Updated test.py

--- test.py
+++ test.py
@@ -1,6 +1,6 @@
 if __name__ == '__main__':
     x = "ECE_180_DA_DB"
-    if x == "EE180_DA_DB":
+    if x == "EE_180_DA_DB":
         print("You are living in 2017")
     else:
         # this is a comment
